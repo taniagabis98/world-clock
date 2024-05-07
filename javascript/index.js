9dc02f2d283200f9faead38e086bf54fe916e7f9
taniagabis98 <t.gabis@experienceco.com>
2024-05-07 16:24:19 +1000

    back to home

--- javascript/index.js
+++ javascript/index.js
@@ -40,7 +40,8 @@ function updateCity(event) {
         <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
       </div>
       <div class="time">${cityTime.format("h:mm:ss [<small>]A[</small>]")}</div>
-    </div>`;
+    </div>
+    <a href="/">All Cities</a>`;
 }
 
 updateTime();
