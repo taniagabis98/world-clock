d7b59d637a4119f6a64ab383475a73be769e9a2f
taniagabis98 <t.gabis@experienceco.com>
2024-05-07 16:19:14 +1000

    Updated JS and current loc

--- javascript/index.js
+++ javascript/index.js
@@ -26,20 +26,21 @@ function updateTime() {
   }
 }
 function updateCity(event) {
-  let cityTimeZone = event.target.value.split("/")[1];
-  let cityName = cityTimeZone.replace("_", " ");
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
- <div class="city">
-          <div>
-            <h2>${cityTimeZone}</h2>
-            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-          </div>
-          <div class="time">${cityTime.format(
-            "h:mm:ss [<small>]A[</small>]"
-          )}</div>
-        </div>`;
+    <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+      <div class="time">${cityTime.format("h:mm:ss [<small>]A[</small>]")}</div>
+    </div>`;
 }
 
 updateTime();
